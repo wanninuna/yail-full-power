68ca53a22d9357b8a77131f9a4a8bec77b052958
wanninuna <wanninuna@gmail.com>
2026-03-04 21:45:39 +0800

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,1 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCbXPYMshMlvl3bEkmtjNmbUEAVq5ytGx0",
+    authDomain: "yali-full-power-644d2.firebaseapp.com",
+    projectId: "yali-full-power-644d2",
+    storageBucket: "yali-full-power-644d2.firebasestorage.app",
+    messagingSenderId: "808812131284",
+    appId: "1:808812131284:web:0da86a0d1d8c01568c9c41"
+  };
 
